6293d103a772489d1ae26c87f225415abdbe83c9
rootgh <95168267+Aliko2020@users.noreply.github.com>
2024-10-10 15:10:18 -1200

    validations

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,20 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 const Signin = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log("onSubmit", values);
+    },
+    validationSchema: yup.object({
+      email: yup.string().required().email('email not valid'),
+      password: yup.string().required().min(8)
+    })
+  });
   return (
-    <form className='flex flex-col gap-4 justify-center items-center py-20'>
-        <div className='flex flex-col justify-center items-center'>
-        <h1 className='text-2xl text-gray-400 font-semibold'>Sign in</h1>
-        <Link to='/signup' className='text-[#5cb95d] cursor-pointer'>Need an account?</Link>
+    <form
+      onSubmit={formik.handleSubmit}
+      className="flex flex-col gap-4 justify-center items-center py-20"
+    >
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-2xl text-gray-400 font-semibold">Sign in</h1>
+        <Link to="/signup" className="text-[#5cb95d] cursor-pointer">
+          Need an account?
+        </Link>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col">
+          <input
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className="appearance-none focus:outline-none w-60 h-10 border px-2 rounded border-gray-300"
+            type="text"
+            placeholder="Email"
+          />
+          <div className="text-sm text-red-600">
+            {formik.errors.email && formik.touched.email && formik.errors.email}
+          </div>
         </div>
-        <div className='flex flex-col gap-4'>
-          <input className="appearance-none focus:outline-none w-60 h-10 border px-2 rounded border-gray-300" type="text" placeholder='Email'/>
-          <input className="appearance-none focus:outline-none w-60 h-10 border px-2 rounded border-gray-300" type="text" placeholder='Password'/>
+        <div className="flex flex-col">
+          <input
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className="appearance-none focus:outline-none w-60 h-10 border px-2 rounded border-gray-300"
+            type="text"
+            placeholder="Password"
+          />
+          <div className="text-sm text-red-600">
+            {formik.errors.password && formik.touched.password && formik.errors.password}
+          </div>
         </div>
-        <button className='px-4 py-3 rounded-sm bg-[#5cb95d] text-white font-semibold' type="button">Sign in</button>
+      </div>
+      <button
+        className="px-4 py-3 rounded-sm bg-[#5cb95d] text-white font-semibold"
+        type="button"
+      >
+        Sign in
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
