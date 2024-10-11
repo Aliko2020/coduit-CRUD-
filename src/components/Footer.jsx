328c2758ded35faa2899a0769558b7ddc4f0a465
rootgh <95168267+Aliko2020@users.noreply.github.com>
2024-10-10 12:26:41 -1200

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex justify-center items-center h-20  bg-gray-200'>
+           <a href="https://github.com/Aliko2020"> <span className='flex justify-center gap-2 items-center'><img className='w-8 h-8' src="https://img.icons8.com/?size=100&id=SzgQDfObXUbA&format=png&color=000000" alt="github logo" />View on Github</span></a>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 
 
@@ -11,6 +12,7 @@ const Layout = () => {
         <Navbar />
         <Header />
         <Outlet />
+        <Footer />
         
     </div>
   )
